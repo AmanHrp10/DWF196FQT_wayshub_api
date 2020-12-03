1da1157df97d1256288cf9d8447433912258a50c
AmanHrp10 <amanhrp.yahoo@gmail.com>
2020-12-03 19:26:56 +0700

    done request get of table channel

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 //? Require Module
-const { getChannelAll, getChannelById } = require('../controllers/channel');
-const { getVideoAll } = require('../controllers/video');
 const { getAllChannels } = require('../controllers/channelSub');
 
-//? Channel Routes
-// router.get('/channels', getChannelAll);
-// router.get('/channel/:id', getChannelById);
-
-//? Video routes
-router.get('/videos', getVideoAll);
-
-//? Channel Subcribe relation routes
 router.get('/channels', getAllChannels);
 
 module.exports = router;
