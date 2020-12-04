39a9ab176c45a9a704bb48150247a9355a62a09e
AmanHrp10 <amanhrp.yahoo@gmail.com>
2020-12-04 19:32:19 +0700

    edit

--- src/controllers/subscribe.js
+++ src/controllers/subscribe.js
@@ -1,1 +1,14 @@
+const { Subscribe, Channel } = require('../../models');
 
+exports.addSubscriber = async (req, res) => {
+  try {
+    const subscribers = await Subscribe.create(body);
+  } catch (err) {
+    res.status(500).send({
+      status: 'Request failed',
+      message: {
+        error: err.message,
+      },
+    });
+  }
+};
