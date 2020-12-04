f135c642a30601b5d7ce5dfe313d8f55a005ef8f
AmanHrp10 <amanhrp.yahoo@gmail.com>
2020-12-05 06:12:07 +0700

    edit

--- src/controllers/subscribe.js
+++ src/controllers/subscribe.js
@@ -1,1 +1,24 @@
+const { Subscribe, Channel } = require('../../models');
 
+exports.addSubscribe = async (req, res) => {
+  try {
+    const { body } = req;
+    const subscribe = await Subscribe.create({
+      channelId: body.channel,
+    });
+
+    res.status(201).send({
+      status: 'Request success',
+      message: 'Data was created',
+      count: subscribe.length,
+      data: {
+        subscribe,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: 'Request failed',
+      message: 'Created subscribe failed',
+    });
+  }
+};
