ae7a669cb823bb035a93df397e3a7ea54fd0f3fb
AmanHrp10 <amanhrp.yahoo@gmail.com>
2020-12-05 14:36:14 +0700

    -

--- src/routes/channel.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getChannelsAll, getChannel } = require('../controllers/channel');
-
-router.get('/channel', getChannelsAll);
-router.get('/channel/:id', getChannel);
-
-module.exports = router;
